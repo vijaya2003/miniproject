3dca21ce38e6d7dcb93eecbf0239c9cbf0235600
vijaya2003 <94471376+vijaya2003@users.noreply.github.com>
2023-08-27 21:31:05 +0530

    Updated server.js file

--- server.js
+++ server.js
@@ -12,139 +12,130 @@ const { db } = require("./firebase.js");
  router.get('/nav', function(req, res) {
     res.render("nav")
   });
-  router.get('/cstudy', function(req, res) {
-    res.render("cstudy")
+  
+router.get('/cfee',async (req,res)=>{
+  try{
+      const usersRef=db.collection("student");
+      const doc=await usersRef.get();
+      let responseArr=[];
+      doc.forEach(ele=>{
+          responseArr.push(ele.data());
+          console.log(ele.data())
+      })
+       res.render('cfee',{responseArr:responseArr});
+      //res.send("hello")
+      console.log(responseArr)
+     
+//res.end()
+  }catch(error){
+      res.send(error);
+  }
+});
+
+
+router.get('/cinternship',async (req,res)=>{
+  try{
+      const usersRef=db.collection("student");
+      const doc=await usersRef.get();
+      let responseArr=[];
+      doc.forEach(ele=>{
+          responseArr.push(ele.data());
+          console.log(ele.data())
+      })
+       res.render('cinternship',{responseArr:responseArr});
+      //res.send("hello")
+      console.log(responseArr)
+     
+//res.end()
+  }catch(error){
+      res.send(error);
+  }
+});
+  
+
+  router.get('/cstudy&conduct',async (req,res)=>{
+    try{
+        const usersRef=db.collection("student");
+        const doc=await usersRef.get();
+        let responseArr=[];
+        doc.forEach(ele=>{
+            responseArr.push(ele.data());
+            console.log(ele.data())
+        })
+         res.render('cstudy&conduct',{responseArr:responseArr});
+        //res.send("hello")
+        console.log(responseArr)
+       
+  //res.end()
+    }catch(error){
+        res.send(error);
+    }
   });
-//   router.get('/cconcern', function(req, res) {
-//     res.render("cconcern")
-//   });
-  router.get('/cfee', function(req, res) {
-    res.render("cfee")
-  });
-  router.get('/cinternship', function(req, res) {
-    res.render("cinternship")
-  });
-  router.get('/cstudy&conduct', function(req, res) {
-    res.render("cstudy&conduct")
-  });
-  // router.get('/ccustodian', function(req, res) {
-  //   res.render("ccustodian")
-  // });
-  router.get('/cresidence', function(req, res) {
-    res.render("cresidence")
+
+
+
+  router.get('/cresidence',async (req,res)=>{
+    try{
+        const usersRef=db.collection("student");
+        const doc=await usersRef.get();
+        let responseArr=[];
+        doc.forEach(ele=>{
+            responseArr.push(ele.data());
+            console.log(ele.data())
+        })
+         res.render('cresidence',{responseArr:responseArr});
+        //res.send("hello")
+        console.log(responseArr)
+       
+  //res.end()
+    }catch(error){
+        res.send(error);
+    }
   });
 
   
 
- router.get('/cstudy', function(req, res) {
-  try{
-    const usersRef=db.collection("student");
-    const doc= usersRef.get();
-    let responseArr=[];
-    doc.forEach(ele=>{
-        responseArr.push(ele.data());
-        console.log(ele.data())
-    })
-     res.render('cstudy',{responseArr:responseArr});
-    //res.send("hello")
-    console.log(responseArr)
-   
-//res.end()
-}catch(error){
-    res.send(error);
-}
+ 
+  router.get('/ccustodian',async (req,res)=>{
+    try{
+        const usersRef=db.collection("student");
+        const doc=await usersRef.get();
+        let responseArr=[];
+        doc.forEach(ele=>{
+            responseArr.push(ele.data());
+            console.log(ele.data())
+        })
+         res.render('ccustodian',{responseArr:responseArr});
+        //res.send("hello")
+        console.log(responseArr)
+       
+  //res.end()
+    }catch(error){
+        res.send(error);
+    }
   });
- router.get('/ccustodian', function(req, res) {
-  try{
-    const usersRef=db.collection("student");
-    const doc= usersRef.get();
-    let responseArr=[];
-    doc.forEach(ele=>{
-        responseArr.push(ele.data());
-        console.log(ele.data())
-    })
-     res.render('ccustodian',{responseArr:responseArr});
-    // res.send("hello")
-    console.log(responseArr)
-   
-//res.end()
-}catch(error){
-    res.send(error);
-}
+
+
+
+  router.get('/cstudy',async (req,res)=>{
+    try{
+        const usersRef=db.collection("student");
+        const doc=await usersRef.get();
+        let responseArr=[];
+        doc.forEach(ele=>{
+            responseArr.push(ele.data());
+            console.log(ele.data())
+        })
+         res.render('cstudy',{responseArr:responseArr});
+        //res.send("hello")
+        console.log(responseArr)
+       
+  //res.end()
+    }catch(error){
+        res.send(error);
+    }
   });
-//  router.get('/cfee', function(req, res) {
-//   try{
-//     const usersRef=db.collection("student");
-//     const doc= usersRef.get();
-//     let responseArr=[];
-//     doc.forEach(ele=>{
-//         responseArr.push(ele.data());
-//         console.log(ele.data())
-//     })
-//      res.render('cfee',{responseArr:responseArr});
-//     //res.send("hello")
-//     console.log(responseArr)
-   
-// //res.end()
-// }catch(error){
-//     res.send(error);
-// }
-//   });
-//  router.get('/cinternship', function(req, res) {
-//   try{
-//     const usersRef=db.collection("student");
-//     const doc= usersRef.get();
-//     let responseArr=[];
-//     doc.forEach(ele=>{
-//         responseArr.push(ele.data());
-//         console.log(ele.data())
-//     })
-//      res.render('cinternship',{responseArr:responseArr});
-//     //res.send("hello")
-//     console.log(responseArr)
-   
-// //res.end()
-// }catch(error){
-//     res.send(error);
-// }
-//   });
-//  router.get('/cresidence', function(req, res) {
-//   try{
-//     const usersRef=db.collection("student");
-//     const doc= usersRef.get();
-//     let responseArr=[];
-//     doc.forEach(ele=>{
-//         responseArr.push(ele.data());
-//         console.log(ele.data())
-//     })
-//      res.render('cresidence',{responseArr:responseArr});
-//     //res.send("hello")
-//     console.log(responseArr)
-   
-// //res.end()
-// }catch(error){
-//     res.send(error);
-// }
-//   });
-//  router.get('/cstudy&conduct', function(req, res) {
-//   try{
-//     const usersRef=db.collection("student");
-//     const doc= usersRef.get();
-//     let responseArr=[];
-//     doc.forEach(ele=>{
-//         responseArr.push(ele.data());
-//         console.log(ele.data())
-//     })
-//      res.render('cstudy&conduct',{responseArr:responseArr});
-//     //res.send("hello")
-//     console.log(responseArr)
-   
-// //res.end()
-// }catch(error){
-//     res.send(error);
-// }
-//   });
-  
+
 
  router.get('/cconcern',async (req,res)=>{
     try{
